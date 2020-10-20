03a909c6bbe432920e8a197a5f9db6713e5feae2
Max Hanson <mthanson2@gmail.com>
2020-10-20 13:18:02 -0700

    Fixed conflicts

--- models/week.js
+++ models/week.js
@@ -1,14 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
-    var Week = sequelize.define('Week', {
-        //add properties here
-        //ex: name: DataTypes.STRING
+    var Week = sequelize.define("Week", {
+      name: DataTypes.RANGE(Sequelize.DATEONLY)
     });
   
     Week.associate = function(models) {
       Week.hasMany(models.Day, {
         onDelete: "cascade"
       }); 
-
     };
+  
     return Week;
-}
+  };
