54698c2c0f6bc91bd2be5c7d035bd1fa4b2c4819
Max Hanson <mthanson2@gmail.com>
2020-10-20 10:23:41 -0700

    Created task table

--- models/task.js
+++ models/task.js
@@ -1,16 +1,47 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Task = sequelize.define("Task", {
-      text: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1, 140]
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        //   might need to change to compare easier to calendar
+        deadline: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        autoschedule: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        timeToComplete: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: true
+        },
+        //   might need to change to compare easier to calendar
+        startline: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        complete: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
         }
-      },
-      complete: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-      }
     });
     return Task;
-  };
+};
