a3556d27e32baf3365128561ed1b6b1e2bbe3a93
Max Hanson <mthanson2@gmail.com>
2020-10-20 13:13:58 -0700

    Put them back in

---
+++ models/task.js
@@ -0,0 +1,52 @@
+module.exports = function (sequelize, DataTypes) {
+    var Task = sequelize.define("Task", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        //   might need to change to compare easier to calendar
+        deadline: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        autoschedule: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        timeToComplete: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: true
+        },
+        //   might need to change to compare easier to calendar
+        startline: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        complete: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+
+    Task.associate = function(models){
+        // add associations here
+        // ex: Task.hasMany(models.BlogPost)
+    };
+    return Task;
+};
