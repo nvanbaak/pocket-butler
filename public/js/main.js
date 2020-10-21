5e8cededb202aa02d868f61ab766210fb478575c
Dan Gentile <dangentile@ymail.com>
2020-10-21 11:07:42 -0700

    fixed time picker

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,7 @@
 // init modals 
 $(document).ready(function() {
     $('.modal').modal();
-});
-
-// init date-picker
-$(document).ready(function() {
+    $('.timepicker').timepicker();
     $('.datepicker').datepicker();
-});
-
-// init select options
-$(document).ready(function() {
     $('select').formSelect();
-});
-
-//Time Picker:
-$('.timepicker').pickatime({
-    default: 'now',
-    twelvehour: false, // change to 12 hour AM/PM clock from 24 hour
-    donetext: 'OK',
-    autoclose: false,
-    vibrate: true // vibrate the device when dragging clock hand
 });
