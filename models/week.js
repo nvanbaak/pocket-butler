285f2ca284a1fbc4edfe733cdc5e3714fa9a770b
Max Hanson <mthanson2@gmail.com>
2020-10-21 10:48:46 -0700

    Changed models to get them working. Hours in days does not work yet so it remains commented out

--- models/week.js
+++ models/week.js
@@ -1,13 +1,49 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var Week = sequelize.define("Week", {
-    name: DataTypes.RANGE(Sequelize.DATEONLY)
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    userid: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    sunday: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    monday: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    tuesday: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    wednesday: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    thursday: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    friday: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    saturday: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   });
 
-  Week.associate = function(models) {
-    Week.hasMany(models.Day, {
-      onDelete: "cascade"
-    }); 
-  };
+  // Week.associate = function(models) {
+  //   Week.hasMany(models.Day, {
+  //     onDelete: "cascade"
+  //   }); 
+  // };
 
   return Week;
 };
