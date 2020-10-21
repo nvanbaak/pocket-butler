832f5f99e004b9e4701e5472a3d57e08ed1733bd
Nikolai <nvanbaak@gmail.com>
2020-10-21 14:08:01 -0700

    added function to parse month codes

--- clock.js
+++ clock.js
@@ -55,6 +55,64 @@ function retainTwoDigits(num) {
     return outputString
 }
 
+function monthString(monthInt) {
+
+    switch (monthInt) {
+        case 0:
+            return "January";
+            break;
+    
+        case 1:
+            return "February";
+            break;
+    
+        case 2:
+            return "March";
+            break;
+    
+        case 3:
+            return "April";
+            break;
+    
+        case 4:
+            return "May";
+            break;
+    
+        case 5:
+            return "June";
+            break;
+
+        case 6:
+            return "July";
+            break;
+    
+        case 7:
+            return "August";
+            break;
+    
+        case 8:
+            return "September";
+            break;
+    
+        case 9:
+            return "October";
+            break;
+    
+        case 10:
+            return "November";
+            break;
+    
+        case 11:
+            return "December";
+            break;
+    
+        default:
+            return "This isn't a real month!";
+            break;
+    
+
+}
+
 // This function takes the number output from getDay and replaces it with a string
 function dayString(dayInt) {
     switch (dayInt) {
