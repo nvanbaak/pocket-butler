50577ac9e05e77993dfcc6ca3ba720f423945c32
“karzoun” <karzoun_27@hotmail.com>
2020-10-20 21:17:46 -0700

    overview

--- models/task.js
+++ models/task.js
@@ -1,7 +1,47 @@
-module.exports = function(sequelize, DataTypes) {
-    var Task = sequelize.define('Task', {
-        //add properties here
-        //ex: name: DataTypes.STRING
+module.exports = function (sequelize, DataTypes) {
+    var Task = sequelize.define("Task", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        //   might need to change to compare easier to calendar
+        deadline: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        autoschedule: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        timeToComplete: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: true
+        },
+        //   might need to change to compare easier to calendar
+        startline: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        complete: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
     });
 
     Task.associate = function(models){
@@ -9,4 +49,4 @@ module.exports = function(sequelize, DataTypes) {
         // ex: Task.hasMany(models.BlogPost)
     };
     return Task;
-}
+};
