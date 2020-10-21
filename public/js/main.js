e159e00cad1d7e9f8dc5c0be7db48ae772eafdcd
Dan Gentile <dangentile@ymail.com>
2020-10-21 13:30:39 -0700

    added full calendar, moment, finished add button, still need to fix mobile responsive

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,41 @@ $(document).ready(function() {
     $('.datepicker').datepicker();
     $('select').formSelect();
 });
+
+// Full Calendar Render
+document.addEventListener('DOMContentLoaded', function() {
+    const calendarEl = document.getElementById('calendar');
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        events: [{
+                title: 'Example sAll Day Event',
+                start: '2020-10-01'
+            },
+            {
+                title: 'Example Long Event',
+                start: '2020-10-07',
+                end: '2020-10-10'
+            },
+            {
+                groupId: '999',
+                title: 'Example Repeating Event',
+                start: '2020-10-09T16:00:00'
+            },
+        ]
+    });
+
+    calendar.render();
+});
+
+
+
+// moment.js
+const currentDate = moment().format('MMMM Do YYYY');
+const currentTime = moment().format('LT');
+$('#date').text(currentDate);
+$('#current-time').text(currentTime);
