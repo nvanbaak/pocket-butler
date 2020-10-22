f77b29cec2a44b94cf11f2150fbf755c2f4634f0
Nikolai <nvanbaak@gmail.com>
2020-10-22 13:02:19 -0700

    wrote function to count time by category for a given week

--- assign.js
+++ assign.js
@@ -1,6 +1,9 @@
 // Dependencies
 fs = require("fs");
 
+// Global variables
+let today = 0; // we're using an integer in the place of a date, but theoretically we could make this work with calendar math
+
 // Classes
 class Week {
     constructor(dayArr, startDate) {
@@ -21,6 +24,7 @@ class Week {
     }
 }
 
+// the User class has a list of tasks, a week template, and a schedule
 class User {
     constructor(weekTemplate) {
         this.tasks = [];
@@ -53,7 +57,11 @@ class User {
     }
 }
 
-
+// Builds and returns a task object
+// name:       name of task
+// category:   used for assigning task start times
+// time:       task start time
+// deadline:   date integer at which the task must be completed
 function newTask(name, category, time, deadline) {
     return {
         "name":name,
@@ -98,6 +106,57 @@ function wageSlave() {
     }
 
     return schedule;
+}
+
+// function to count how much time is available in each category
+function getAllCategoryTimes(weekObj) {
+
+    // set up output list
+    // This list will contain objects with a "name" property and a "count" property
+    let catList = [];
+
+    // Run through the week object by days
+    for (let i = 0; i < weekObj.length; i++) {
+
+        // For each day, run through the hours
+        for (let j = 0; j < weekObj[i].length; j++) {
+
+            // Locate the index for that category in the list, if any
+            // Start with an index at -1
+            let catIndex = -1;
+
+            // Run through the list of categories
+            for (let k = 0; k < catList.length; k++) {
+
+                // This statement requires some explanation:
+                // For each index in the category list, we get the name of that category
+                // Then we check it against the string in the week object, which should also be a category string
+                if (catList[k].name === weekObj[i][j]) {
+
+                    // If they're the same, reset the index
+                    catIndex = k; 
+                    
+                    // then increment the count and break the loop
+                    catList[k].count++;
+                    break;
+                }
+            }
+
+            // if the category we want isn't in the list...
+            if (catIndex > -1) {
+
+                // push a new category object to the list
+                catList.push({
+                    "name":weekObj[i][j],
+                    "count":1
+                });
+            }
+        }
+    }
+
+    // Having walked through the whole week, return the list
+    return catList;
+
 }
 
 
