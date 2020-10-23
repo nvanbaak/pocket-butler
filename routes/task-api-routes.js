c5cbb6be94dfe8b70f7306f0dc4b550cfdcc3e11
Nikolai <nvanbaak@gmail.com>
2020-10-23 13:58:45 -0700

    tasks populate in the list now

--- routes/task-api-routes.js
+++ routes/task-api-routes.js
@@ -20,7 +20,8 @@ module.exports = function(app) {
 
     app.post("/api/tasks", function(req, res) {
         db.Task.create(req.body).then(function(newTask) {
-            res.json(newTask);
+            // res.json(newTask);
+            res.redirect("/dashboard");
         })
     });
 
