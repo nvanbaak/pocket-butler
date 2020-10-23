114946a7664bc75922ee1fdb02c59f30fdf35762
Darrian DeVaughnn <devondevaughnn@gmail.com>
2020-10-23 10:31:43 -0700

    reconnected server and sendText/sendEmail

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ require("./routes/authentication-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 require("./routes/task-api-routes.js")(app);
-require("./sendEmail")(app)
+//require("./sendEmail")(app)
 //require("./sendText")(app)
 
 
