db4953f64670246798211ad42bfaa18fb58464c6
Dan Gentile <dangentile@ymail.com>
2020-10-27 07:58:41 -0700

    removed frontend elements that aren't ready yet

--- models/week.js
+++ models/week.js
@@ -1,25 +1,25 @@
-const db = require(".");
+// const db = require(".");
 
-module.exports = function(sequelize, DataTypes) {
-    const Week = sequelize.define("Week", {
-        date: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        days: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        }
+// module.exports = function(sequelize, DataTypes) {
+//     const Week = sequelize.define("Week", {
+//         date: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         days: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         }
 
-    });
+//     });
 
-    Week.associate = function(models) {
-        Week.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
+//     Week.associate = function(models) {
+//         Week.belongsTo(models.User, {
+//             foreignKey: {
+//                 allowNull: false
+//             }
+//         });
+//     };
 
-    return Week;
-};
+//     return Week;
+// };
